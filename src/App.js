a9a8d71434b9f0ac6fa84b2a5fbb7ab6e7018282
desaurii <evgeniya.dolgova.00@gmail.com>
2026-04-21 19:22:43 +0300

    remove unused useState from App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-import { useState } from "react";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Main from "./components/Main/Main";
