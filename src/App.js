960df13528c3d802c1ac59a292378dda3882e212
desaurii <evgeniya.dolgova.00@gmail.com>
2026-04-23 19:46:23 +0300

    feat: add routing and users page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Main from "./components/Main/Main";
+import Users from "./pages/Users/Users";
 
 function App() {
   return (
-    <div className="app">
-      <Header />
+    <BrowserRouter basename="/take-home-assessment-react-1">
+      <div className="app">
+        <Header />
 
-      <Main />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/users" element={<Users />} />
+        </Routes>
 
-      <Footer />
-    </div>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
